e08c7cc809e7c37772aae5068a4e033324451b71
Mohamed Hesham <101864682+MohamedBoghdaddy@users.noreply.github.com>
2024-05-10 23:47:51 +0300

    Update Bedrooms.js

--- src/Components/Products/Bedrooms.js
+++ src/Components/Products/Bedrooms.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
