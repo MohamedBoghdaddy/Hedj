4c1b71d871d8a5b6078e605cc9fc30779356d612
Mohamed Hesham <101864682+MohamedBoghdaddy@users.noreply.github.com>
2024-05-10 23:49:25 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import from "react-router-dom"; // Correct context
+import { BrowserRouter } from "react-router-dom"; // Correct context
 import App from "./App";
 
 test("renders learn react link", () => {
