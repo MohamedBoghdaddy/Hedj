6e61e0165d84652b01f55d83b2f2130a9fcc9470
Mohamed Hesham <101864682+MohamedBoghdaddy@users.noreply.github.com>
2024-05-12 13:49:54 +0300

    Update customerslist.jsx

--- src/Components/dashboard/customerslist.jsx
+++ src/Components/dashboard/customerslist.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../Styles/lists.css";
-import employeeimage from '../../Assets/Images/simple.jpg.jpg';
+import employeeimage from '../../Assets/Images/simpl.jpg';
 
 
 const customers=[
