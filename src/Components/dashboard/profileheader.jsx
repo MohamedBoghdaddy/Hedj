33c25da8705f380c1a72eb1afa874a28f503af17
bahig2004 <162214341+bahig2004@users.noreply.github.com>
2024-05-19 22:26:47 +0300

    Update profileheader.jsx

--- src/Components/dashboard/profileheader.jsx
+++ src/Components/dashboard/profileheader.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import {BiEdit} from "react-icons/bi"
-const profileheader = ()=>{
-  return ( 
-        <div className="proheader">
-            <h2 className="profiletitle">Profile</h2>
-            <div className="edit">
-                <BiEdit className="icon"/>
-            </div>
-        </div>
+import { BiEdit } from "react-icons/bi";
+
+
+const Profileheader = ({ isCollapsed }) => {
+  return (
+    <div className={`proheader ${isCollapsed ? 'collapsed' : ''}`}>
+      <h2 className="profiletitle">{!isCollapsed && 'Profile'}</h2>
+      <div className="edit">
+        <BiEdit className="icon" />
+      </div>
+    </div>
   );
 };
-export default profileheader;
+
+export default Profileheader;
