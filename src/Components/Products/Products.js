ca924d6abd762a3b7afaf233b775249963e9333d
rehajibrahim <rehajibrahimm@gmail.com>
2024-05-21 22:56:25 +0300

    dropdown menu for the products

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,8 +3,9 @@ import "../../Styles/Products.css";
 import { Link } from "react-router-dom";
 
 function Products() {
+  const Menus=['Profile', "apps", "settings"];
   return (
-    <div className="container">
+    /*<div className="container">
       <ul className="first">
         <li>News 2024</li>
         <li>KITCHEN</li>
@@ -26,7 +27,21 @@ function Products() {
           </li>
         </ul>
       </div>
-    </div>
+    </div>*/
+
+<div className="dropdown">
+  <ul>
+    {
+      Menus.map((menu)=>(
+
+        <li key={menu}>{menu}</li>
+      ))
+    }
+  </ul>
+</div>
+
+
+
   );
 }
 
