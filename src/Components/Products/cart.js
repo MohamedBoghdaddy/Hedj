1ad03ea853bee359f541bd02c7e1b52c7fd72406
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-08-25 23:06:01 +0300

    Update cart.js

--- src/Components/Products/cart.js
+++ src/Components/Products/cart.js
@@ -2,65 +2,71 @@ import React from "react";
 import "../../Styles/Products.css";
 import "../../Styles/cart.css";
 
-
-const Cart = () => {
-
-const Cart = ({ cart }) => {
+const Cart = ({ cart = [] }) => {
   // Function to calculate the total number of products in the cart
   const calculateTotalProducts = () => {
     return cart.reduce((total, item) => total + (item.quantity || 1), 0);
   };
 
-
-  return(
+  return (
     <>
-    <section className="section">
-      <div className="container">
-       <div className="row">
-        <div className="col-md-8">
-        <h3 className="hd mb-0 mt-5">Your Cart</h3>
-        <p>There are <b>3</b> products in your cart</p>
-        <div className="table-responsive">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Unit Price</th>
-                <th>Quantity</th>
-                <th>Subtotal</th>
-                <th>Remove</th>
-              </tr>
-            </thead>
-          </table>
-          <tbody>
-            <tr>
-              <td>
-<div className="d-flex align-items-center cartItemimgWrapper">
-  <div className="imgWrapper2">
-    <img src="https://i.pinimg.com/736x/c7/26/55/c726555b47cf7fae4bd96e7e1cdcee66.jpg" className="images"/>
-  </div>
-</div>
-              </td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
+      <section className="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <h3 className="hd mb-0 mt-5">Your Cart</h3>
+              <p>
+                There are <b>{calculateTotalProducts()}</b> products in your
+                cart
+              </p>
+              <div className="table-responsive">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>Product</th>
+                      <th>Unit Price</th>
+                      <th>Quantity</th>
+                      <th>Subtotal</th>
+                      <th>Remove</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cart.map((item, index) => (
+                      <tr key={index}>
+                        <td>
+                          <div className="d-flex align-items-center cartItemimgWrapper">
+                            <div className="imgWrapper2">
+                              <img
+                                src={item.image}
+                                alt={item.name}
+                                className="images"
+                              />
+                            </div>
+                            <span>{item.name}</span>
+                          </div>
+                        </td>
+                        <td>{item.price}</td>
+                        <td>{item.quantity || 1}</td>
+                        <td>
+                          {(item.price * (item.quantity || 1)).toFixed(2)}
+                        </td>
+                        <td>
+                          <button className="btn btn-danger">Remove</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="col-md-4">
+              {/* Add any additional content like a summary of the cart total */}
+            </div>
+          </div>
         </div>
-        </div>
-        <div className="col-md-4">
-
-        </div>
-       </div>
-      </div>
-    </section>
-    
+      </section>
     </>
-  )
-
-
- 
+  );
 };
 
-export default Cart
+export default Cart;
