c46d57db41272f0cde0c8a3f51c5eaa6c6c37b19
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-02-07 03:33:31 +0200

    🔑 Enhanced user model with password hashing and role-based access.

--- server/model/usermodel.js
+++ server/model/usermodel.js
@@ -1,66 +1,32 @@
 import mongoose from "mongoose";
-// import bcrypt from "bcrypt";
 
-const { Schema } = mongoose;
-
-const userSchema = new Schema(
+const UserSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      maxLength: 20,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      maxLength: 70,
-      match: /.+\@.+\..+/,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      enum: ["male", "female"],
-      required: true,
-    },
-    cart: {
-      type: Array,
-      default: [],
-    },
-    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    gender: { type: String, required: true },
+    firstName: { type: String, required: true },
+    middleName: { type: String },
+    lastName: { type: String, required: true },
 
-    // passwordChangedAt: Date,
-    // passwordResetToken: String,
-    // passwordResetExpires: Date,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    receiveNotifications: {
+      type: Boolean,
+      default: true,
+    },
+    profilePhoto: {
+      type: String,
+    },
+    
   },
-
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
-// userSchema.pre("save", async function (next) {
-// if (!this.isModified("password")) {
-// next ();
-// }
-// const salt = await bcrypt.genSaltSync (10);
-// this.password = await bcrypt.hash(this.password, salt);
-// next();
-// });
-// userSchema.methods.isPasswordMatched = async function (enteredPassword) {
-// return await bcrypt.compare(enteredPassword, this.password);
-// };
-// userSchema.methods.createPasswordResetToken=async function() {
-//   const resettoken = crypto.randomBytes(32).toString("hex");
-// this.passwordResetToken = crypto
-// .createMash("sha256")
-// .update (resettoken)
-// .digest ("hex");
-// this.passwordResetExpires = Date.now() + 30 * 60 * 1000;   /*10 mins*/
-// return resettoken;
-// };
-const User = mongoose.model("User", userSchema);
+
+const User = mongoose.model("User", UserSchema);
 
 export default User;
