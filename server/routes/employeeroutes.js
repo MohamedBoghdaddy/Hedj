0222f135c3f771ad334e9c31c26bad32d1a19e34
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-02-07 03:33:45 +0200

    🛠️ Updated employee routes for improved API endpoints and security.

--- server/routes/employeeroutes.js
+++ server/routes/employeeroutes.js
@@ -7,21 +7,6 @@ import {
   updateEmployee,
   getLastAdmins,
 } from "../controller/employeecontroller.js";
-import {
-  createUser,
-  loginUser,
-  logoutUser,
-  getAllUsers,
-  updateUser,
-  deleteUser,
-} from "../controller/usercontroller.js";
-import {
-  createProduct,
-  getProducts,
-  getProductById,
-  updateProduct,
-  deleteProduct,
-} from "../controller/productscontroller.js";
 
 const router = express.Router();
 
@@ -30,16 +15,6 @@ router.get("/getall", getAllEmployees);
 router.get("/getone/:id", getEmployee);
 router.put("/update/:id", updateEmployee);
 router.delete("/delete/:id", deleteEmployee);
-router.post("/users/signup", createUser);
-router.post("/users/login", loginUser);
-router.post("/users/logout", logoutUser);
-router.get("/users/customerslist", getAllUsers);
-router.put("/users/update/:id", updateUser); // Add this line
-router.get("/getlastadmins", getLastAdmins);
-router.delete("/users/:id", deleteUser);
-router.post("/Products", createProduct);
-router.get("/Products", getProducts);
-router.get("/Products/:id", getProductById);
-router.put("/Products/:id", updateProduct);
-router.delete("Products/:id", deleteProduct);
+router.get("/lastadmins", getLastAdmins);
+
 export default router;
