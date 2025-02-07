7445d3834e33fcadbeea942496f164eeacc4a7fc
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-02-07 04:55:14 +0200

    🔄 Updated entry point to integrate new context providers and improved state management.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom"; // Import createRoot function from react-dom
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { AuthProvider } from "./context/AuthContext";
+import { ShopContextProvider } from "./context/productContext";
 const root = createRoot(document.getElementById("root")); // Use createRoot function
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
